e41ebd4037965be341fea5f17b6152fb3ab28bbb
Jorge Rivadeneira <jorge.rivadeneira.cevallos@gmail.com>
2022-01-10 14:53:03 -0500

    First Commit

---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const {Router} = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+    return res.json('All users sent');
+});
+
+/* Get specific user*/
+router.get('/:id', (req, res) => {
+    if(req.params.id == 'U0001'){
+        return res.json('User U0001 Found');
+    }
+    return res.status(404).json('User not found');
+});
+
+/* Add user */
+router.post('/', (req, res) => {
+    const {username, password} = req.body;
+    if(username && password){
+        return res.status(201).json('User created');
+    }
+    res.status(400).json('user not created');
+});
+
+
+module.exports = router;
